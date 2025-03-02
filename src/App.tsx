ed72b4fe764b1ad3ce7552fa08a5f5a926078a71
Jonas Andersen <Jonas@anders1.com>
2025-03-02 01:32:10 +0100

    UPT: Last one for today (Hero Section)

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,32 @@ import Result from "./components/Result";
 function App() {
   return (
     <div className="app">
-      <div className="drawing-container">
-        <Toolbar />
-        <Canvas />
+      <div>
+        <h1>🎨 Test Your Drawing Against a Trained CNN Model 🧠</h1>
+        <p>
+          This project features a{" "}
+          <div className="highlight">Convolutional Neural Network (CNN)</div>{" "}
+          build from{" "}
+          <div className="highlight">scratch</div> trained on{" "}
+          <div className="highlight">X predefined classes</div>. Your task is to
+          draw one of these classes, and the model will analyze your drawing to
+          identify the closest match.{" "}
+          <div className="highlight">The goal is to evaluate</div> how well the
+          model can recognize and classify your input based on its training.{" "}
+          <br />
+          Challenge the model and see how accurately it can predict your
+          drawing!
+        </p>
       </div>
-      <div className="result-container">
-        <Result />
+      <div style={{ display: "flex", gap: "1rem" }}>
+        <div className="drawing-container">
+          <Toolbar />
+
+          <Canvas />
+        </div>
+        <div className="result-container">
+          <Result />
+        </div>
       </div>
     </div>
   );
